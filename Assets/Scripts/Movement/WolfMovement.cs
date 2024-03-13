d5eba4cf2153f057f71d9fddd31a13ba95e80f4a
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-03-13 20:25:56 +0000

    Add files via upload

---
+++ Assets/Scripts/Movement/WolfMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class WolfMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 2.5f;
+    new Rigidbody2D rigidbody;
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() => rigidbody.velocity = new Vector2(speed, 0);
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        /* Based on the below, with modifications 
+        // @Credit: https://www.gamedev.tv/courses/1394720/lectures/34692227
+        // Part of the https://www.gamedev.tv/p/unity-2d-game-dev-course-2021 course
+        // Note that the video is not accessible unless enrolled to the above 
+        */
+
+        if (other.CompareTag("Wall"))
+        {
+            speed = -speed;
+            FlipWolf();
+        }
+    }
+
+    void FlipWolf()
+    {
+        /* Based on the below, with modifications 
+        // @Credit: https://www.gamedev.tv/courses/1394720/lectures/34692227
+        // Part of the https://www.gamedev.tv/p/unity-2d-game-dev-course-2021 course
+        // Note that the video is not accessible unless enrolled to the above 
+        */
+        transform.localScale = new Vector2((-Mathf.Sign(rigidbody.velocity.x) / 2.0f), 0.5f);
+    }
+}
