a1dfcb3eee8d224bc94644559be994d9cc2bcbec
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-06-18 01:48:04 +0100

    Create word_cloud.py

---
+++ Evaluation/word_cloud.py
@@ -0,0 +1,24 @@
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+# create an array of adjectives
+words = ["lovely", "good", "cartoony", "nice", "cute", "great", "nice", "nice", "fun", "playable", "challenging", "good", "mature", "fun", 
+"unique", "good", "playable", "good", "fast-paced", "good", "responsive", "challenging", "right"]
+
+# join the array with any empty spaces 
+words = " ".join(words)
+# print(words)
+
+# set stopwords 
+stopwords = set(STOPWORDS)
+
+# set up the word cloud 
+wordcloud = WordCloud(background_color ='beige', stopwords = stopwords).generate(words)
+ 
+# plot the word cloud image                       
+plt.figure(figsize = (8, 8), facecolor = None)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.tight_layout(pad = 0)
+ 
+plt.show()
