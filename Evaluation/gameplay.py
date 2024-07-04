70961a486f22b6b50773b4f28d8c98447d9bde42
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-04 23:32:52 +0200

    Update gameplay.py

--- Evaluation/gameplay.py
+++ Evaluation/gameplay.py
@@ -1,22 +1,48 @@
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
-# create an array of adjectives
-gameplay = ["accessible", "fun", "playable", "challenging", "good", "mature", "fun", "alright", "unique", "good", "playable", "good", "fast-paced", "good", "responsive", "challenging", "right", "fine", "good", "good"]
+# create an array of adjectives and other descriptive words
+gameplay = [
+    "accessible",
+    "fun",
+    "playable",
+    "challenging",
+    "good",
+    "mature",
+    "fun",
+    "better gameplay experience and challenge",
+    "nice variety of enemies to face",
+    "feels like a prototype",
+    "alright",
+    "excellent job showing damage",
+    "game idea itself is excellent",
+    "unique",
+    "good",
+    "playable",
+    "good",
+    "fast-paced",
+    "good",
+    "responsive",
+    "challenging",
+    "right",
+    "fine",
+    "good",
+    "good",
+]
 
-# join the array with any empty spaces 
+# join the array with any empty spaces
 gameplay = " ".join(gameplay)
 
-# set stopwords 
+# set stopwords
 stopwords = set(STOPWORDS)
 
-# set up the word cloud 
-wordcloud = WordCloud(background_color ='white', stopwords = stopwords).generate(gameplay)
- 
-# plot the WordCloud image                       
-plt.figure(figsize = (7, 7), facecolor = None)
-plt.imshow(wordcloud, interpolation='bilinear')
+# set up the word cloud
+wordcloud = WordCloud(background_color="white", stopwords=stopwords).generate(gameplay)
+
+# plot the WordCloud image
+plt.figure(figsize=(7, 7), facecolor=None)
+plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
-plt.tight_layout(pad = 0)
- 
+plt.tight_layout(pad=0)
+
 plt.show()
