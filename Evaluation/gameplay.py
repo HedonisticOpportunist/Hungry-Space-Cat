c5b78760d70d638f0a7d599b73bf2443400275c0
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-04 23:14:43 +0200

    Update gameplay.py

--- Evaluation/gameplay.py
+++ Evaluation/gameplay.py
@@ -2,7 +2,7 @@ from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
 # create an array of adjectives
-gameplay = ["fun", "playable", "challenging", "good", "mature", "fun", "unique", "good", "playable", "good", "fast-paced", "good", "responsive", "challenging", "right"]
+gameplay = ["accessible", "fun", "playable", "challenging", "good", "mature", "fun", "alright", "unique", "good", "playable", "good", "fast-paced", "good", "responsive", "challenging", "right", "fine", "good", "good"]
 
 # join the array with any empty spaces 
 gameplay = " ".join(gameplay)
