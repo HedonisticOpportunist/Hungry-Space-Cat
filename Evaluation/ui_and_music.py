74bcfeb5b379b5c86de513b92e175819f9c6d240
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-04 23:28:02 +0200

    Update ui_and_music.py

--- Evaluation/ui_and_music.py
+++ Evaluation/ui_and_music.py
@@ -1,22 +1,24 @@
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
-# create an array of adjectives
-ui_and_music = ["great", "nice", "nice"]
+# create an array of adjectives and other descriptive words 
+ui_and_music = ["great", "nice", "good", "nice"]
 
-# join the array with any empty spaces 
+# join the array with any empty spaces
 ui_and_music = " ".join(ui_and_music)
 
-# set stopwords 
+# set stopwords
 stopwords = set(STOPWORDS)
 
-# set up the word cloud 
-wordcloud = WordCloud(background_color ='cyan', stopwords = stopwords).generate(ui_and_music)
- 
-# plot the WordCloud image                       
-plt.figure(figsize = (4, 4), facecolor = None)
-plt.imshow(wordcloud, interpolation='bilinear')
+# set up the word cloud
+wordcloud = WordCloud(background_color="cyan", stopwords=stopwords).generate(
+    ui_and_music
+)
+
+# plot the WordCloud image
+plt.figure(figsize=(4, 4), facecolor=None)
+plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
-plt.tight_layout(pad = 0)
- 
+plt.tight_layout(pad=0)
+
 plt.show()
