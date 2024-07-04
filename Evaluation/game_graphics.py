681b7b487d449ab90ba75184d241d32411c4ff36
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-04 23:24:41 +0200

    Update game_graphics.py

--- Evaluation/game_graphics.py
+++ Evaluation/game_graphics.py
@@ -1,22 +1,24 @@
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
 
-# create an array of adjectives
-graphics = ["lovely", "good", "cartoony", "nice", "cute"]
+# create an array of adjectives and other descriptive words
+graphics = ["lovely", "palette feels like velvet", "good", "cartoony", "nice", "cute"]
 
-# join the array with any empty spaces 
+# join the array with any empty spaces
 graphics = " ".join(graphics)
 
-# set stopwords 
+# set stopwords
 stopwords = set(STOPWORDS)
 
-# set up the word cloud 
-wordcloud = WordCloud(background_color ='lightblue', stopwords = stopwords).generate(graphics)
- 
-# plot the WordCloud image                       
-plt.figure(figsize = (6, 6), facecolor = None)
-plt.imshow(wordcloud, interpolation='bilinear')
+# set up the word cloud
+wordcloud = WordCloud(background_color="lightblue", stopwords=stopwords).generate(
+    graphics
+)
+
+# plot the WordCloud image
+plt.figure(figsize=(6, 6), facecolor=None)
+plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
-plt.tight_layout(pad = 0)
- 
+plt.tight_layout(pad=0)
+
 plt.show()
