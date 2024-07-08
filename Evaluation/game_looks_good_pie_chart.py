4cefb0c4a7063aa7492bcc9573303c692d39114b
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-09 00:03:34 +0200

    Update game_looks_good_pie_chart.py

--- Evaluation/game_looks_good_pie_chart.py
+++ Evaluation/game_looks_good_pie_chart.py
@@ -7,6 +7,8 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Credit: https://www.w3schools.com/python/matplotlib_pie_charts.asp
+
 y = np.array([3, 1])
 mylabels = ["Looks good", "Partially looks good"]
 
