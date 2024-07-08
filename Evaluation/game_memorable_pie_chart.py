e7b8482ed8f0ae40e8fecc2b2a98b7c55d0c5e95
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-09 00:03:59 +0200

    Update game_memorable_pie_chart.py

--- Evaluation/game_memorable_pie_chart.py
+++ Evaluation/game_memorable_pie_chart.py
@@ -7,6 +7,8 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Credit: https://www.w3schools.com/python/matplotlib_pie_charts.asp
+
 y = np.array([2, 2])
 mylabels = ["Memorable", "Partially memorable"]
 
