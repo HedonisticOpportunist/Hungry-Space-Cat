998f6015de6e37d4f73c82962de81070c38cbfd5
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-16 22:38:02 +0200

    Update game_memorable_pie_chart.py

--- Evaluation/game_memorable_pie_chart.py
+++ Evaluation/game_memorable_pie_chart.py
@@ -10,7 +10,7 @@ import numpy as np
 # Credit: https://www.w3schools.com/python/matplotlib_pie_charts.asp
 
 y = np.array([4, 5, 1, 1])
-mylabels = ["Memorable", "Partially Memorable", "N/A", "Partially unmemorable]
+mylabels = ["Memorable", "Partially Memorable", "N/A", "Partially Unmemorable"]
 
 plt.pie(y, labels=mylabels, startangle=90)
 plt.show()
