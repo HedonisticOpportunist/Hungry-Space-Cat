a30c3acdcdd6d2a200271018f3245b23a83ed2c4
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-16 22:33:28 +0200

    Update gameplay_pie_chart.py

--- Evaluation/gameplay_pie_chart.py
+++ Evaluation/gameplay_pie_chart.py
@@ -9,8 +9,8 @@ import numpy as np
 
 # Credit: https://www.w3schools.com/python/matplotlib_pie_charts.asp
 
-y = np.array([4])
-mylabels = ["Game is playable"]
+y = np.array([8, 3, 2])
+mylabels = ["Playable", "Partially Playable", "N/A"]
 
 plt.pie(y, labels=mylabels, startangle=90)
 plt.show()
