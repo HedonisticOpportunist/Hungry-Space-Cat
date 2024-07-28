118272debc3c2c58d91bdd503c77cde9a6ea4250
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-07-28 16:00:02 +0200

    Add files via upload

--- Assets/Scripts/GamePlay/DealWithEffects.cs
+++ Assets/Scripts/GamePlay/DealWithEffects.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class DealWithEffects : MonoBehaviour
 {
-
     public static bool backGroundEffectsEnabled = true;
     public static bool spriteEffectsEnabled = true;
 
     public void DisableMovingBackgroundEffect() => backGroundEffectsEnabled = false;
     public void EnableMovingBackgroundEffect() => backGroundEffectsEnabled = true;
-    public void DisableSpriteEffects() => backGroundEffectsEnabled = false;
-    public void EnableSpriteEffects() => backGroundEffectsEnabled = true;
+    public void DisableSpriteEffects() => spriteEffectsEnabled = false;
+    public void EnableSpriteEffects() => spriteEffectsEnabled = true;
 }
