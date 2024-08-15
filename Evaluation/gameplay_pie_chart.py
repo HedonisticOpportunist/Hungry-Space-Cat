f9320f55fd853ab9215246a067dfe08adaa0d35b
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-08-16 00:38:56 +0200

    Update gameplay_pie_chart.py

--- Evaluation/gameplay_pie_chart.py
+++ Evaluation/gameplay_pie_chart.py
@@ -9,6 +9,11 @@ import numpy as np
 
 # Credit: https://www.w3schools.com/python/matplotlib_pie_charts.asp
 
+# Week 14
+# y = np.array([8, 2, 1])
+# mylables= ["Playable", "Partially Playable" "N/A"]
+
+# Week 18 
 y = np.array([1, 6])
 mylabels = ["Partially Playable", "Playable"]
 
