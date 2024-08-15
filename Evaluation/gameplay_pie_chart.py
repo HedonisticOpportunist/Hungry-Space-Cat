357c1af91fa20add7452c10fe9be82a5227a0c7c
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-08-16 00:25:42 +0200

    Update gameplay_pie_chart.py

--- Evaluation/gameplay_pie_chart.py
+++ Evaluation/gameplay_pie_chart.py
@@ -9,7 +9,7 @@ import numpy as np
 
 # Credit: https://www.w3schools.com/python/matplotlib_pie_charts.asp
 
-y = np.array([3, 4])
+y = np.array([1, 6])
 mylabels = ["Partially Playable", "Playable"]
 
 plt.pie(y, labels=mylabels, startangle=90)
