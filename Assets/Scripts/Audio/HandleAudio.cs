b83d1afc331f754844fb3b79b6cf65b206ea6407
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-08-15 23:52:24 +0200

    Add files via upload

--- Assets/Scripts/Audio/HandleAudio.cs
+++ Assets/Scripts/Audio/HandleAudio.cs
@@ -1,7 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
+
+/* Handle volume implementation, based on the below: 
+// @Credit:https://www.youtube.com/watch?v=yWCHaTwVblk
+*/
+
 public class HandleAudio : MonoBehaviour
 {
-    public void MuteSound() => AudioListener.volume = 0;
+    [SerializeField] Slider sliderVolume;
 
-    public void PlayAudio() => AudioListener.volume = 1;
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("audioVolume"))
+        {
+            PlayerPrefs.SetFloat("audioVolume", 0.5f);
+            LoadValue();
+        }
+        else
+        {
+            LoadValue();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = sliderVolume.value;
+        SaveValue();
+    }
+
+    void LoadValue()
+    {
+        sliderVolume.value = PlayerPrefs.GetFloat("audioVolume");
+    }
+
+    void SaveValue()
+    {
+        PlayerPrefs.SetFloat("audioVolume", sliderVolume.value);
+    }
 }
