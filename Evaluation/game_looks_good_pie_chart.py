1c49fab4bb57c1b7cea4e2452edaba594113a1fe
HedonisticOpportunist <fujitahibaraki@gmail.com>
2024-08-16 00:34:14 +0200

    Update game_looks_good_pie_chart.py

--- Evaluation/game_looks_good_pie_chart.py
+++ Evaluation/game_looks_good_pie_chart.py
@@ -9,7 +9,8 @@ import numpy as np
 
 # Credit: https://www.w3schools.com/python/matplotlib_pie_charts.asp
 
-y = np.array([5, 2])
+# y = np.array([8, 2, 1]) # week 14; laste category denotes "Neither agree or disagree"
+y = np.array([5, 2]) # week 18 
 mylabels = ["Looks Good", "Partially Looks Good"]
 
 plt.pie(y, labels=mylabels, startangle=90)
